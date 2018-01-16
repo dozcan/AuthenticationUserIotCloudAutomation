4c7ca47f20a729d184cce369334eb75e90a8128e
dozcan <dogasava@gmail.com>
2018-01-16 23:34:53 +0300

    config

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,8 @@ namespace Auth
             */
             //_connectionString = $@"Server={_config["MYSQL_SERVER_NAME"]}; Database={_config["MYSQL_DATABASE"]}; Uid={_config["MYSQL_USER"]}; Pwd={_config["MYSQL_PASSWORD"]}";
       
-            _connectionString = "Server=0.0.0.0;Database=iot;Port=3306;User=root;Password=105481Do";
+            _connectionString = "Server=172.31.31.14;Database=iot;Port=3306;User=root;Password=105481Do";
+            
       
             String config = _connectionString;//Configuration["ConnectionStrings:DefaultConnection".ToString()];
             _host[0] = "172.31.30.175";
